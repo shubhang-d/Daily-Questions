064a35117c490e8a61eab659b204139eb3e4f207
Shubhang Dixit <143957984+shubhang-d@users.noreply.github.com>
2025-08-13 12:19:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (48.44%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int Camera = 0;
+    public int minCameraCover(TreeNode root) {
+        int c = minCamera(root);
+        if(c==-1){
+            Camera++;
+        }
+        return Camera;
+    }
+
+    public int minCamera(TreeNode root){
+        if(root == null) return 0;
+        int left = minCamera(root.left);
+        int right = minCamera(root.right);
+        if(left == -1 || right == -1){
+            Camera++;
+            return 1;
+        }else if(left == 1 || right == 1){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+}
