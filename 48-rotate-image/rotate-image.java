27b3bcf337be68aca31a8cb0906f9301bedcb669
Shubhang Dixit <143957984+shubhang-d@users.noreply.github.com>
2025-09-29 22:44:36 +0530

    Time: 10 ms (2.12%) | Memory: 42.5 MB (15.02%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,35 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        transposematrix(matrix);
-        reverserows(matrix);     
+        transpose(matrix);
+        swap(matrix);
     }
-    private void transposematrix(int[][] matrix)
-    {
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<=i;j++)
-            {
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+    public void transpose(int[][] matrix){
+        for(int i =0; i<matrix.length;i++){
+            for(int j = i+1;j<matrix[0].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-
-    }
-    private void reverserows(int[][] matrix)
-    {
-        for(int i=0;i<matrix.length;i++)
-        {
-            int left=0;
-            int right=matrix.length-1;
-            while(left<right)
-            {
-                int temp=matrix[i][left];
-                matrix[i][left]=matrix[i][right];
-                matrix[i][right]=temp;
-                left++;
-                right--;
+        for(int[] a: matrix){
+            for(int l: a){
+                System.out.print(l);
             }
+            System.out.println();
+        }
+    }
+    public void swap(int[][] matrix){
+        int left = 0;
+        int right = matrix[0].length-1;
+        while(left<=right){
+            for(int i = 0;i<matrix.length;i++){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+            left++;
+            right--;
         }
     }
 }
