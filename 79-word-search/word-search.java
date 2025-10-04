cf3653561a4e18590785e54d437496d6c25a4b86
Shubhang Dixit <143957984+shubhang-d@users.noreply.github.com>
2025-10-04 07:59:13 +0530

    Time: 294 ms (8.65%) | Memory: 45 MB (20.66%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,32 +2,34 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i =0 ;i<board.length;i++){
             for(int j = 0;j<board[0].length;j++){
-                boolean check = findNext(board, word, 0, i, j);
-                if(check){
-                    return true;
+                if(board[i][j] == word.charAt(0)){
+                    if(check(board, word, 0, i, j)){
+                        return true;
+                    }
                 }
             }
         }
         return false;
-        
     }
-    public boolean findNext(char[][] board, String word, int ch, int x, int y){
-        if(ch == word.length()){
+    public boolean check(char[][] board, String word, int idx, int i, int j){
+        if(idx == word.length()){
             return true;
         }
-        if(x<0 || y<0 || x>=board.length || y>=board[0].length || word.charAt(ch) != board[x][y] || board[x][y] == '*'){
+        if(i<0 || j<0 || i>=board.length || j>= board[0].length || board[i][j] == '*' || word.charAt(idx) != board[i][j]){
             return false;
         }
-        
-        char temp = board[x][y];
-        board[x][y] = '*';
-        if (findNext(board, word, ch+1, x, y+1) ||
-        findNext(board, word, ch+1, x+1, y) ||
-        findNext(board, word, ch+1, x, y-1) ||
-        findNext(board, word, ch+1, x-1, y)){
-            return true;
+
+        int[][] dir = {{0,1}, {-1,0}, {0, -1}, {1,0}};
+
+        for(int k =0; k<dir.length;k++){
+            char temp = board[i][j];
+            board[i][j] = '*';
+            if(check(board, word, idx+1, i+dir[k][0], j+dir[k][1])){
+                return true;
+            }
+            board[i][j] = temp;
         }
-        board[x][y] = temp;
         return false;
     }
+
 }
