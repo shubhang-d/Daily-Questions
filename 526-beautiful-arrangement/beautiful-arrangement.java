d0aa284df0dff8e07e9ba947303d8d69fe162773
Shubhang Dixit <143957984+shubhang-d@users.noreply.github.com>
2025-10-06 10:13:40 +0530

    Time: 76 ms (11.93%) | Memory: 40 MB (97.33%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -5,7 +5,7 @@ class Solution {
         if (index == nums.size()) {
             ans += 1;
         } else {
-            for (int i = index; i < nums.size(); ++i) {
+            for (int i = index; i < nums.size(); i++) {
                 if ((index+1) % nums.get(i) == 0 || nums.get(i) % (index+1) == 0) {
                     Collections.swap(nums, i, index);
                     dfs(index+1, nums);
