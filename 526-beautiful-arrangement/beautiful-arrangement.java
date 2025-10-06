cb47fd26ab87f8794e3f5d23dc6b32569da24184
Shubhang Dixit <143957984+shubhang-d@users.noreply.github.com>
2025-10-06 09:46:27 +0530

    Time: 76 ms (11.93%) | Memory: 40 MB (97.33%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -0,0 +1,24 @@
+class Solution {
+    int ans = 0;
+    
+    public void dfs(Integer index, List<Integer> nums) {
+        if (index == nums.size()) {
+            ans += 1;
+        } else {
+            for (int i = index; i < nums.size(); ++i) {
+                if ((index+1) % nums.get(i) == 0 || nums.get(i) % (index+1) == 0) {
+                    Collections.swap(nums, i, index);
+                    dfs(index+1, nums);
+                    Collections.swap(nums, i, index);
+                }
+            }
+        }
+    }
+    
+    public int countArrangement(int n) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) nums.add(i);
+        dfs(0, nums);
+        return ans;
+    }
+}
